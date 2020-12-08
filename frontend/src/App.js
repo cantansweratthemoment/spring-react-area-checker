bbf81130588682716d5b7a4470f4a7e4010efb01
Olya <ilinskayaov@yandex.ru>
2020-12-08 05:35:19 +0300

    Зачатки фронтенда🍳
    1. Котофон.
    2. Авторы.
    3. Плейсхолдер для формы логина.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import Heading from "./first_page_components/heading";
+import Login from "./first_page_components/login";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+    return (<div className="firstPage" style={{
+        backgroundImage: "url(wallpaper.JPEG)",
+        backgroundSize: "45%",
+        backgroundRepeat: "repeat"
+    }}><Heading/><br/>
+    <Login/>
+    </div>)
 }
 
 export default App;
