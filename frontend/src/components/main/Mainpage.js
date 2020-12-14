8f12cee50f19eef6ef9d0fadf81cf6718fa2d3b6
Olya <ilinskayaov@yandex.ru>
2020-12-14 22:17:52 +0300

    Обновление таблички сразу при заходе на главную страницу

--- frontend/src/components/main/Mainpage.js
+++ frontend/src/components/main/Mainpage.js
@@ -1,24 +1,44 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Logout from "./Logout";
 import CoordinatesForm from "./Form";
 import Resulttable from "./Table";
 import Graph from "./Graph";
 import "./main.css"
+import store from "../../app/store";
 
 function Mainpage() {
+    const [x_form, setX] = useState('0');
+    const [y_form, setY] = useState('0');
+    const [r_form, setR] = useState('1');
+    const [checks, setChecks] = useState('null');
+    useEffect(() => {
+        let information = {
+            "login": store.getState().login
+        };
+        let body = [];
+        for (const inf in information) {
+            body.push(inf + "=" + information[inf]);
+        }
+        if (checks === null) {
+            fetch("/all" + body, {
+                method: "POST"
+            }).then(response => response.text().then(text => {
+                console.log(JSON.parse(text));
+                setChecks(JSON.parse(text));
+            }))
+        }
+    })
+
     const checkNumbers = (q, a, b) => {
         return ((q > a) && (q < b));
     }
     const validate = () => {
         return (checkNumbers(x_form, -3, 3) && checkNumbers(y_form, -3, 3) && checkNumbers(r_form, 0, 3));
     }
-    const showChecks=()=>{
+    const showChecks = () => {
         console.log(checks);
     }
-    const [x_form, setX] = useState('0');
-    const [y_form, setY] = useState('0');
-    const [r_form, setR] = useState('1');
-    const [checks, setChecks] = useState('null');
+
     return (<div>
         <Logout/>
         <Graph r={r_form} setChecks={setChecks} checks={checks}/>
